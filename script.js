d5255be58a8c29237d2322bd9bebe29d66ea2a60
s0lix-x <damaryuswira4@gmail.com>
2026-03-11 21:31:06 +0700

    Add Loading State

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ let profile = document.getElementById("profile");
 
 cari.addEventListener("click", (usernameGithub) => {
   let username = document.getElementById("username").value;
-  profile.innerHTML = "";
-
+  profile.innerHTML = "Loading...";
   async function ambilData() {
     try {
       let response = await fetch(`https://api.github.com/users/${username}`);
@@ -13,6 +12,7 @@ cari.addEventListener("click", (usernameGithub) => {
         return;
       }
       let data = await response.json();
+      profile.innerHTML = "";
       profile.innerHTML += `<img src=${data.avatar_url}><br>`;
       profile.innerHTML += `Username: ${data.name}<br>`;
       profile.innerHTML += `Public Repos: ${data.public_repos}<br>`;
